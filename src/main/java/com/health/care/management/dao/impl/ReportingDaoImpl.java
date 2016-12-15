c48230bedd0598e44cda6135e41caa887a9cc92a
srhsdpgroup <r.naik46@yahoo.in>
2016-12-15 01:04:06 +0100

    code formating,
    changing the datatype to decimal

--- src/main/java/com/health/care/management/dao/impl/ReportingDaoImpl.java
+++ src/main/java/com/health/care/management/dao/impl/ReportingDaoImpl.java
@@ -24,13 +24,15 @@ public class ReportingDaoImpl implements ReportingDao {
     public String generateInventoryReport() {
         JasperReportBuilder report = DynamicReports.report();
         String inventoryFilePath = "f:/inventory_report.pdf";
-        report.columns(Columns.column("Inventory Id", "inventory_id", DataTypes.integerType()), Columns.column("Inventory Name", "name", DataTypes.stringType()),
-                Columns.column("Description", "description", DataTypes.stringType()), Columns.column("quantity", "quantity", DataTypes.integerType()),
-                Columns.column("Price per Unit", "price_per_unit", DataTypes.integerType()), Columns.column("Vendor Contact Info", "vendor_contact", DataTypes.stringType()),
+        report.columns(Columns.column("Inventory Id", "inventory_id", DataTypes.integerType()), 
+                Columns.column("Inventory Name", "name", DataTypes.stringType()),
+                Columns.column("Description", "description", DataTypes.stringType()),
+                Columns.column("quantity", "quantity", DataTypes.integerType()),
+                Columns.column("Price per Unit", "price_per_unit", DataTypes.doubleType()), 
+                Columns.column("Vendor Contact Info", "vendor_contact", DataTypes.stringType()),
                 Columns.column("Type of Inventory", "type", DataTypes.stringType()))
-                .title(// title of the report
-                        Components.text("Inventory List").setHorizontalAlignment(HorizontalAlignment.CENTER))
-                .pageFooter(Components.pageXofY())// show page number on the page footer
+                .title(Components.text("Inventory List").setHorizontalAlignment(HorizontalAlignment.CENTER))
+                .pageFooter(Components.pageXofY())
                 .setDataSource(Constant.FETCH_ALL_INVENTORIES, DataSourceUtils.getConnection(HealthCareServiceConfiguration.getJdbcConnection().getDataSource()));
 
         return generateReport(report, inventoryFilePath);
@@ -40,14 +42,17 @@ public class ReportingDaoImpl implements ReportingDao {
     public String generateListOfDoctorReport() {
         JasperReportBuilder report = DynamicReports.report();
         String listOfAllDoctorFilePath = "f:/listOfAllDoctor_report.pdf";
-        report.columns(Columns.column("Registration Id", "registration_id", DataTypes.integerType()), Columns.column("First Name", "first_name", DataTypes.stringType()),
-                Columns.column("Last Name", "last_name", DataTypes.stringType()), Columns.column("Specialization", "specialization", DataTypes.stringType()),
-                Columns.column("Department", "department", DataTypes.stringType()), Columns.column("Qualification", "qualification", DataTypes.stringType()),
-                Columns.column("Mobile Number", "mobile_no", DataTypes.integerType()), Columns.column("Email", "email", DataTypes.stringType()),
+        report.columns(Columns.column("Registration Id", "registration_id", DataTypes.integerType()), 
+                Columns.column("First Name", "first_name", DataTypes.stringType()),
+                Columns.column("Last Name", "last_name", DataTypes.stringType()),
+                Columns.column("Specialization", "specialization", DataTypes.stringType()),
+                Columns.column("Department", "department", DataTypes.stringType()),
+                Columns.column("Qualification", "qualification", DataTypes.stringType()),
+                Columns.column("Mobile Number", "mobile_no", DataTypes.bigDecimalType()),
+                Columns.column("Email", "email", DataTypes.stringType()),
                 Columns.column("Status", "status", DataTypes.stringType()))
-                .title(// title of the report
-                        Components.text("List of all the doctors").setHorizontalAlignment(HorizontalAlignment.CENTER))
-                .pageFooter(Components.pageXofY())// show page number on the page footer
+                .title(Components.text("List of all the doctors").setHorizontalAlignment(HorizontalAlignment.CENTER))
+                .pageFooter(Components.pageXofY())
                 .setDataSource(Constant.FETCH_ALL_DOCTOR, DataSourceUtils.getConnection(HealthCareServiceConfiguration.getJdbcConnection().getDataSource()));
         return generateReport(report, listOfAllDoctorFilePath);
     }
@@ -59,17 +64,20 @@ public class ReportingDaoImpl implements ReportingDao {
     public String generateDetailsOfAllPatient() {
         JasperReportBuilder report = DynamicReports.report();
         String listOfAllPatientFilePath = "f:/listOfAllPatient_report.pdf";
-        report.columns(Columns.column("Patient Id", "patient_id", DataTypes.integerType()), Columns.column("Patient First Name", "p_first_name", DataTypes.stringType()),
-                Columns.column("Patient Last Name", "p_last_name", DataTypes.stringType()), Columns.column("Patient Date of Birth", "date_of_birth", DataTypes.dateType()),
+        report.columns(Columns.column("Patient Id", "patient_id", DataTypes.integerType()),
+                Columns.column("Patient First Name", "p_first_name", DataTypes.stringType()),
+                Columns.column("Patient Last Name", "p_last_name", DataTypes.stringType()),
+                Columns.column("Patient Date of Birth", "date_of_birth", DataTypes.dateType()),
                 Columns.column("Patient mentioned Illness", "illness", DataTypes.stringType()),
                 Columns.column(" Doctor Registration Id", "registration_id", DataTypes.integerType()),
-                Columns.column("Doctor First Name", "d_first_name", DataTypes.stringType()), Columns.column("Doctor Last Name", "d_last_name", DataTypes.stringType()),
+                Columns.column("Doctor First Name", "d_first_name", DataTypes.stringType()), 
+                Columns.column("Doctor Last Name", "d_last_name", DataTypes.stringType()),
                 Columns.column("Doctor Specialization", "specialization", DataTypes.stringType()),
-                Columns.column("Department", "department", DataTypes.stringType()), Columns.column("Mobile Number", "mobile_no", DataTypes.integerType()),
+                Columns.column("Department", "department", DataTypes.stringType()),
+                Columns.column("Mobile Number", "mobile_no", DataTypes.bigDecimalType()),
                 Columns.column("Doctor Treatment prescription", "prescription", DataTypes.stringType()), Columns.column("Diagnosis Status", "status", DataTypes.stringType()))
-                .title(// title of the report
-                        Components.text("List of all the Patients treated").setHorizontalAlignment(HorizontalAlignment.CENTER))
-                .pageFooter(Components.pageXofY())// show page number on the page footer
+                .title(Components.text("List of all the Patients treated").setHorizontalAlignment(HorizontalAlignment.CENTER))
+                .pageFooter(Components.pageXofY())
                 .setDataSource(Constant.LIST_OF_ALL_PATIENT, DataSourceUtils.getConnection(HealthCareServiceConfiguration.getJdbcConnection().getDataSource()));
 
         return generateReport(report, listOfAllPatientFilePath);
@@ -80,13 +88,19 @@ public class ReportingDaoImpl implements ReportingDao {
     public String generateReportOfSinglePatient(int patientId) {
         JasperReportBuilder report = DynamicReports.report();
         String detailsOfSinglePatientFilePath = "f:/" + patientId + "patientDetailsById_report.pdf";
-        report.columns(Columns.column("Patient Id", "patient_id", DataTypes.integerType()), Columns.column("Patient First Name", "p_first_name", DataTypes.stringType()),
-                Columns.column("Patient Last Name", "p_last_name", DataTypes.stringType()), Columns.column("Patient Date of Birth", "date_of_birth", DataTypes.dateType()),
+        report.columns(Columns.column("Patient Id", "patient_id", DataTypes.integerType()),
+                Columns.column("Patient First Name", "p_first_name", DataTypes.stringType()),
+                Columns.column("Patient Last Name", "p_last_name", DataTypes.stringType()),
+                Columns.column("Patient Date of Birth", "date_of_birth", DataTypes.dateType()),
                 Columns.column("Patient mentioned Illness", "illness", DataTypes.stringType()),
-                Columns.column(" Doctor Registration Id", "registration_id", DataTypes.integerType()), Columns.column("Doctor First Name", "d_first_name", DataTypes.stringType()),
-                Columns.column("Doctor Last Name", "d_last_name", DataTypes.stringType()), Columns.column("Doctor Specialization", "specialization", DataTypes.stringType()),
-                Columns.column("Department", "department", DataTypes.stringType()), Columns.column("Mobile Number", "mobile_no", DataTypes.integerType()),
-                Columns.column("Doctor Treatment prescription", "prescription", DataTypes.stringType()), Columns.column("Diagnosis Status", "status", DataTypes.stringType()))
+                Columns.column(" Doctor Registration Id", "registration_id", DataTypes.integerType()),
+                Columns.column("Doctor First Name", "d_first_name", DataTypes.stringType()),
+                Columns.column("Doctor Last Name", "d_last_name", DataTypes.stringType()), 
+                Columns.column("Doctor Specialization", "specialization", DataTypes.stringType()),
+                Columns.column("Department", "department", DataTypes.stringType()),
+                Columns.column("Mobile Number", "mobile_no", DataTypes.bigDecimalType()),
+                Columns.column("Doctor Treatment prescription", "prescription", DataTypes.stringType()), 
+                Columns.column("Diagnosis Status", "status", DataTypes.stringType()))
                 .title(// title of the report
                         Components.text("List of all the Patients treated").setHorizontalAlignment(HorizontalAlignment.CENTER))
                 .pageFooter(Components.pageXofY())// show page number on the page footer
@@ -109,7 +123,7 @@ public class ReportingDaoImpl implements ReportingDao {
     }
 
     private String getQueryForPatient(int PatientId) {
-        String getPatientDetailsById=  String.format(Constant.LIST_OF_SINGLE_PATIENT_DETAIL,PatientId);
+        String getPatientDetailsById = String.format(Constant.LIST_OF_SINGLE_PATIENT_DETAIL, PatientId);
         return getPatientDetailsById;
     }
 }
